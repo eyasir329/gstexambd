f8c5c2850a845c002a7d7857efdc21eccaec1ed5
eyasir329 <202004017@shu.edu.bd>
2023-01-28 08:25:49 +0600

    compare marks to answer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 char answer(char ans0[],int num);
+int total(char arr1[],char arr2[],int num);
 
 
 
@@ -8,6 +9,10 @@ int main(){
     int qnum=125;
     char ans[]="ACDCBCCACBCBDBCCBCCDCBBBACCDACBCAAACBBBCDBBDBBBCBBACBCBBACCDBCDACAACCBBCDDBCCBAABBDDBCBABBDBCCCAABCBBBACBCCCCCAAABABAABCCCABD";
     answer(ans,qnum);
+
+    char marks[]={"BCACBCCACBCBDBCCBCCDCBBBACCDACBCAAACBBBCDBBDBBBCBBACBCBBACCDBCDACAACCBBCDDBCCBAABBDDBCBABBDBCCCAABCBBBACBCCCCCAAABABAABCCCABD"};
+
+    total(ans,marks,qnum);
 }
 
 
@@ -18,4 +23,19 @@ char answer(char ans0[],int num){
     for(i=0;i<num;i++){
         printf("%d is %c\n",i+1,ans0[i]);
     }
+}
+
+//compare marks to answer and count total
+int total(char arr1[],char arr2[],int num){
+    int i,count=0,cut=0;
+    float total0=0;
+    for(i=0;i<num;i++){
+        if(arr1[i]==arr2[i]){
+            count++;
+        }else{
+            cut++;
+        }
+    }
+    total0=(float)count-((0.25)*(float)cut);
+    printf("total is %.2f\n",total0);
 }
