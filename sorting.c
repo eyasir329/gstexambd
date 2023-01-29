f0311924ff19419cb27a282331f77864bb62a955
eyasir329 <202004017@shu.edu.bd>
2023-01-30 02:35:12 +0600

    almost finish

--- sorting.c
+++ sorting.c
@@ -6,15 +6,17 @@
 #define Num_Student 15
 
 struct student{
-    char roll[MaxLines][MaxLength];
-    char name[MaxLines][MaxLength];
-    char marks[MaxLines][MaxLength];
+    int rollNum;
+    char studName[MaxLines][MaxLength];
+    int MarksALL;
 };
 
 int main(){
-    int i,line=0
-    ;
+    int i,j,line=0;
     char arrTotal[MaxLines][MaxLength];
+    char roll[MaxLines][MaxLength];
+    char marks[MaxLines][MaxLength];
+
     FILE *fin;
     fin=fopen("total.text","r");
     if(fin==NULL){
@@ -32,18 +34,22 @@ int main(){
 
     //structure
     int na=1;
-    int ro=3;
-    int to=5;
+    int ro=3,r1;
+    int to=5,t1;
     struct student input[Num_Student];
     for(i=0;i<Num_Student;i++){
 
-        strcpy(input[i].name,arrTotal[na]);
+        strcpy(input[i].studName,arrTotal[na]);
         na=na+6;
 
-        strcpy(input[i].roll,arrTotal[ro]);
+        strcpy(roll[i],arrTotal[ro]);
+        r1=atoi(roll[i]);
+        input[i].rollNum=r1;
         ro=ro+6;
 
-        strcpy(input[i].marks,arrTotal[to]);
+        strcpy(marks[i],arrTotal[to]);
+        t1=atoi(marks[i]);
+        input[i].MarksALL=t1;
         to=to+6;
     }
 
@@ -52,57 +58,36 @@ int main(){
     printf("-----------------------------------------------------------\n");
     printf("Display List Without Sorting\n");
     printf("-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
     for(i=0;i<Num_Student;i++){
-        printf("Name : %sRoll : %sMarks : %s\n",input[i].name,input[i].roll,input[i].marks);
+        printf("\nName : %sRoll : %d\nMarks : %d\n",input[i].studName,input[i].rollNum,input[i].MarksALL);
     }
     printf("-----------------------------------------------------------\n");
 
 
     //sorting structure
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-/*
-    //name
-    printf("Printing Name From File\n");
-    int n=1;
-    while (n<MaxLines)
-    {
-        printf("%s",arrTotal[n]);
-        n=n+6;
-    }
-    
-
-
-    //roll
-    printf("Printing Roll From File\n");
-    int r=3;
-    while (r<MaxLines)
-    {
-        printf("%s",arrTotal[r]);
-        r=r+6;
-    }
-    
-    //total
-    printf("Printing Total Number From File\n");
-    int t=5;
-    while (t<MaxLines)
-    {
-        printf("%s",arrTotal[t]);
-        t=t+6;
+    struct student temp;
+    for(i=0;i<Num_Student-1;i++){
+        for(j=0;j<(Num_Student-1-i);j++){
+            if(input[j].MarksALL<input[j+1].MarksALL){
+                temp=input[j];
+                input[j]=input[j+1];
+                input[j+1]=temp;
+            }
+        }
     }
 
-    */
+    //display after sorting
+    //display struct
+    printf("-----------------------------------------------------------\n");
+    printf("\t\t\tMerit List\n");
+    printf("-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
+    for(i=0;i<Num_Student;i++){
+        printf("\nName : %sRoll : %d\nMarks : %d\n",input[i].studName,input[i].rollNum,input[i].MarksALL);
+    }
+    printf("-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
+
     return 0;
 }
