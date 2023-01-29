4325c6cf8462f3d454dacf2d49627d40baf757de
eyasir329 <202004017@shu.edu.bd>
2023-01-29 20:59:10 +0600

    sorting

--- sorting.c
+++ sorting.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 #define MAX_LINES 1000
 #define lineNEED 90
 #define MAX_LENG 30
 #define STUDENT 30
 
 int main(){
-    int i,line=0;
+    int i,line=1;
     char numArr[MAX_LINES][MAX_LENG];
     char name[500];
     int roll[100],total[100];
@@ -17,15 +18,15 @@ int main(){
         printf("\t\t----------------------------------------------------\n\t\t\tTotal Number File Not Found\n\t\t----------------------------------------------------\n");
         return 1;
     }
-    while (!feof(fin) && !ferror(fin))
+    while (!feof(fin))
     {
-        if (fgets(numArr[line], MAX_LENG, fin) != NULL)
-        {
-            line++;
-        }
+        fscanf(fin,"%s",&name[line-0]);
+        line=line+6;
     }
     fclose(fin);
 
+
+/*
     //printing total number and other info from file
     printf("\t\t----------------------------------------------------\n");
     printf("\t\t\tWe found data from file :\n");
@@ -36,14 +37,42 @@ int main(){
     printf("----------------------------------------------------------------------------\n");
     printf("----------------------------------------------------------------------------\n");
 
+    //all name print
+    printf("--------name--------\n");
+    i=1;
+    while(i<line){
+        printf("%s",numArr[i]);
+        i=i+6;
+    }
+    //roll
+    printf("--------roll--------\n");
+    i=3;
+    while(i<line){
+        printf("%s",numArr[i]);
+        i=i+6;
+    }
+    //total
+    printf("--------total--------\n");
+    i=5;
+    while(i<line){
+        printf("%s",numArr[i]);
+        i=i+6;
+    }
+int k=(int)numArr[3];
+printf("%d",k);
     //input name from numARR to name
-    for(i=0;i<STUDENT;i++){
-        name[i]=numArr[i+1]
-        break;
-    }
-    //checking
-    for(i=0;i<STUDENT;i++){
-        name[i]=numArr[i+1]
-    }
+    
+
+
+*/
+
+
+
+
+
+
+
+
+
     return 0;
 }
